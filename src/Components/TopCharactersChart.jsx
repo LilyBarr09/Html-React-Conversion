9859942db6bcb2b4bf9d692a7781b4b80b9ccb75
Liliana Barrera <barrera.jimnlil@gmail.com>
2023-06-21 15:19:05 -0400

    According to feedback: in the TopCharacterRatings component I deleted the filter method and used sort and slice instead to sort and display top five characters with the most votes

--- src/Components/TopCharactersChart.jsx
+++ src/Components/TopCharactersChart.jsx
@@ -1,6 +1,9 @@
 import "../CSS/CharacterRatings.css";
 
 export function TopCharactersChart({ data }) {
+  const sortedVotes = data
+    .sort((v1, v2) => (v1.votes < v2.votes ? 1 : v1.votes > v2.votes ? -1 : 0))
+    .slice(0, 5);
   return (
     <div>
       <section id="character-ratings">
@@ -11,22 +14,20 @@ export function TopCharactersChart({ data }) {
             <th>Skillset</th>
             <th>Votes</th>
           </tr>
-          {data
-            .filter((item) => item.votes >= 90)
-            .map((item, itemIndex) => {
-              const adjustedIndex = itemIndex + 1;
-              const isOdd = adjustedIndex % 2 === 1;
-              return (
-                <tr
-                  key={item.name}
-                  className={`item ${isOdd ? "dark" : "light"}`}
-                >
-                  <td key={item.name}>{item.name}</td>
-                  <td key={item.skillSet}>{item.skillSet}</td>
-                  <td key={item.votes}>{item.votes}</td>
-                </tr>
-              );
-            })}
+          {sortedVotes.map((item, itemIndex) => {
+            const adjustedIndex = itemIndex + 1;
+            const isOdd = adjustedIndex % 2 === 1;
+            return (
+              <tr
+                key={item.name}
+                className={`item ${isOdd ? "dark" : "light"}`}
+              >
+                <td key={item.name}>{item.name}</td>
+                <td key={item.skillSet}>{item.skillSet}</td>
+                <td key={item.votes}>{item.votes}</td>
+              </tr>
+            );
+          })}
         </table>
       </section>
     </div>
